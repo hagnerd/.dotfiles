bec0780ab18c7b9faa75eb7219c81d4bd224d22b
Matt Hagner <web@matthagner.com>
2020-02-12 11:21:01 -0600

    Pull out zsh fzf autocomplete config

--- zsh/post/syntax-highlighting.zsh
+++
@@ -1,5 +0,0 @@
-source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-
-# IF you receive "highlighters directory not found" error message,
-# you may need to add the following to your .zshenv:
-# export ZSH_HIGHLIGHT_HIGHLIGHTERS_DIR=/usr/local/share/zsh-syntax-highlighting/highlighters
